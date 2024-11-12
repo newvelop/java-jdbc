2289b1f3ea6bf909be1d92d3b8ef9f29f8360bbe
newvelop <newvelop@gmail.com>
2024-11-12 23:51:22 +0900

    refactor : preparestatementsetter 구현

--- jdbc/src/main/java/camp/nextstep/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/camp/nextstep/jdbc/core/JdbcTemplate.java
@@ -20,35 +20,32 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public void insert(String sql, List<Object> params) {
+    public void insert(String sql, Object... params) {
         Connection conn = getConnection();
         runUpdateQuery(conn, sql, params);
     }
 
-    public void update(String sql, List<Object> params) {
+    public void update(String sql, Object... params) {
         Connection conn = getConnection();
         runUpdateQuery(conn, sql, params);
     }
 
-    public void delete(String sql, List<Object> params) {
+    public void delete(String sql, Object... params) {
         Connection conn = getConnection();
         runUpdateQuery(conn, sql, params);
     }
 
-    public <T> T queryForObject(final String sql, final RowMapper<T> rm) {
-        return queryForObject(sql, rm, new ArrayList<>());
-    }
-
-    public <T> T queryForObject(final String sql, final RowMapper<T> rm, final List<Object> params) {
+    public <T> T queryForObject(final String sql, final RowMapper<T> rm, final Object... params) {
         final var list = query(sql, rm, params);
         return list == null || list.isEmpty() ? null : list.get(0);
     }
 
-    public <T> List<T> query(final String sql, final RowMapper<T> rm) {
-        return query(sql, rm, new ArrayList<>());
-    }
-
-    public <T> List<T> query(final String sql, final RowMapper<T> rm, final List<Object> params) {
+    public <T> List<T> queryWithSetter(
+            final String sql,
+            final RowMapper<T> rm,
+            final PreparedStatementSetter preparedStatementSetter,
+            final Object... params
+    ) {
         PreparedStatement pstmt = null;
         Connection conn = getConnection();
         List<T> result = new ArrayList<>();
@@ -56,9 +53,7 @@ public class JdbcTemplate {
             pstmt = conn.prepareStatement(sql);
 
             log.debug("query : {}", sql);
-            for (int i = 0; i < params.size(); i++) {
-                pstmt.setString(i + 1, params.get(i).toString());
-            }
+            preparedStatementSetter.setValues(pstmt);
             final var resultSet = pstmt.executeQuery();
             while (true) {
                 try {
@@ -90,15 +85,53 @@ public class JdbcTemplate {
         }
     }
 
-    private void runUpdateQuery(Connection conn, String sql, List<Object> params) {
+    public <T> List<T> query(final String sql, final RowMapper<T> rm, final Object... params) {
+        PreparedStatement pstmt = null;
+        Connection conn = getConnection();
+        List<T> result = new ArrayList<>();
+        try {
+            pstmt = conn.prepareStatement(sql);
+
+            log.debug("query : {}", sql);
+            defaultPreparedStatementSetter(params).setValues(pstmt);
+            final var resultSet = pstmt.executeQuery();
+            while (true) {
+                try {
+                    if (!resultSet.next()) break;
+                    result.add(rm.mapRow(resultSet));
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    break;
+                }
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException ignored) {
+            }
+
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ignored) {
+            }
+            return result;
+        }
+    }
+
+    private void runUpdateQuery(Connection conn, String sql, Object... params) {
         PreparedStatement pstmt = null;
         try {
             pstmt = conn.prepareStatement(sql);
 
             log.debug("query : {}", sql);
-            for (int i = 0; i < params.size(); i++) {
-                pstmt.setString(i + 1, params.get(i).toString());
-            }
+            defaultPreparedStatementSetter(params).setValues(pstmt);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
@@ -126,5 +159,19 @@ public class JdbcTemplate {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
         }
+    }
+
+    private PreparedStatementSetter defaultPreparedStatementSetter(Object... args) throws SQLException {
+        return ps -> {
+            try {
+                for (int i = 0; i < args.length; i++) {
+                    ps.setObject(i + 1, args[i]);
+                }
+            } catch (NullPointerException e) {
+                throw new SQLException(e);
+            } catch (SQLException e) {
+                throw new SQLException(e);
+            }
+        };
     }
 }
